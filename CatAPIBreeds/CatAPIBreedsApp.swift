b68f7e7791d27c9dffe0b9afcd31441555e4506f
helderduartez <helderduarte.colgaia@gmail.com>
2025-06-18 21:50:14 +0100

    Initial Commit

---
+++ CatAPIBreeds/CatAPIBreedsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatAPIBreedsApp.swift
+//  CatAPIBreeds
+//
+//  Created by Helder Duarte on 18/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct CatAPIBreedsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
