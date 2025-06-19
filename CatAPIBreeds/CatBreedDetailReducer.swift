b4d93107251c08bb7fa5c61754333da558cff76d
helderduartez <helderduarte.colgaia@gmail.com>
2025-06-19 23:58:44 +0100

    +Added breed detail view

---
+++ CatAPIBreeds/CatBreedDetailReducer.swift
@@ -0,0 +1,8 @@
+//
+//  CatBreedDetailReducer.swift
+//  CatAPIBreeds
+//
+//  Created by Helder Duarte on 19/06/2025.
+//
+
+import Foundation
---
+++ CatAPIBreeds/CatBreedDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  CatBreedDetailView.swift
+//  CatAPIBreeds
+//
+//  Created by Helder Duarte on 19/06/2025.
+//
+
+import SwiftUI
+
+struct CatBreedDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CatBreedDetailView()
+}
